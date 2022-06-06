892958f34d991eec7f40f954f09505690cf7d1eb
Puja <contact.pujachaudhury@gmail.com>
2022-06-06 12:11:43 +0530

    added github

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,22 +1,20 @@
 import React from "react";
-
+import { AiFillGithub } from "react-icons/ai";
 function Footer() {
   return (
-    <footer class="p-4 bg-transparent rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 border-t border-grayy">
-      <span class="text-l text-white sm:text-center dark:text-gray-400">
-        © 2022{" "}
-        {/* <a href="https://flowbite.com" class="hover:underline"> */}
-          EVM Security
-        {/* </a> */}
-        . All Rights Reserved.
+    <footer class=" p-4 bg-transparent rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 border-t border-grayy">
+      <span class="text-sm text-white sm:text-center dark:text-gray-400">
+        © 2022 {/* <a href="https://flowbite.com" class="hover:underline"> */}
+        EVM Security
+        {/* </a> */}. All Rights Reserved.
       </span>
-      <ul class="flex flex-wrap items-center mt-3 text-sm text-white dark:text-gray-400 sm:mt-0">
-        {/* <li>
-          <a href="#" class="mr-4 hover:underline md:mr-6 ">
-            About
+      <ul class=" hidden md:flex flex-wrap items-center mt-3 text-sm text-white dark:text-gray-400 sm:mt-0">
+        <li>
+          <a href="https://github.com/evm-security" target="_blank" class="mr-6 hover:underline md:mr-24 ">
+            <AiFillGithub style={{fontSize:'2rem'}} />
           </a>
         </li>
-        <li>
+        {/* <li>
           <a href="#" class="mr-4 hover:underline md:mr-6">
             Privacy Policy
           </a>
